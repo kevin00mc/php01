8c8393efae80cd40232c6075d3f6ad5967c78561
kevin00mc <t.nagaoka1001@gmail.com>
2023-01-16 00:09:21 +0900

    mibae henko

--- php_kadai01/index.php
+++ php_kadai01/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MIL ペット派閥　アンケート入力</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <h1>MIL ペット派閥　アンケートフォーム</h1>
